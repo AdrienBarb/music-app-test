e0b71b127fd30b394847d31307d249ecac7f792e
adrienBarb <adrien-barbier@hotmail.fr>
2023-08-22 17:07:41 +0200

    add logi to play song

--- src/components/Music/MusicPlayer.js
+++ src/components/Music/MusicPlayer.js
@@ -1,14 +1,80 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../../styles/MusicPlayer.module.scss";
 import {
   faBackward,
   faForward,
+  faPause,
   faPlay,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MusicPlayer = ({ music }) => {
-  console.log(music?.color?.[0]);
+  const audioRef = useRef(null);
+
+  const [duration, setDuration] = useState(0);
+  const [current, setCurrent] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.addEventListener("loadedmetadata", () => {
+        setDuration(audioRef.current.duration);
+      });
+
+      audioRef.current.addEventListener("timeupdate", () => {
+        setCurrent(audioRef.current.currentTime);
+      });
+    }
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.removeEventListener("loadedmetadata", () => {});
+        audioRef.current.removeEventListener("timeupdate", () => {});
+      }
+    };
+  }, [music]);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.addEventListener("ended", () => {
+        setIsPlaying(false);
+      });
+
+      return () => {
+        audioRef.current.removeEventListener("ended", () => {});
+      };
+    }
+  }, []);
+
+  const togglePlayback = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleProgressBarClick = (e) => {
+    const width = e.target.clientWidth;
+    const offsetX = e.nativeEvent.offsetX;
+    const percentage = offsetX / width;
+    const newCurrentTime = duration * percentage;
+
+    audioRef.current.currentTime = newCurrentTime;
+    setCurrent(newCurrentTime);
+  };
+
+  const formatTime = (seconds) => {
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = Math.floor(seconds % 60);
+
+    // Convertit les chiffres en format 00
+    if (minutes < 10) minutes = "0" + minutes;
+    if (remainingSeconds < 10) remainingSeconds = "0" + remainingSeconds;
+
+    return minutes + ":" + remainingSeconds;
+  };
 
   return (
     <div className={styles.container}>
@@ -26,7 +92,26 @@ const MusicPlayer = ({ music }) => {
           <div className={styles.musicArtist}>{music?.artist}</div>
         </div>
 
-        <div className={styles.player}></div>
+        <div className={styles.player}>
+          <audio
+            ref={audioRef}
+            src={music?.audio}
+            controls
+            autoPlay
+            style={{ display: "none" }}
+          />
+          <div className={styles.time}>{formatTime(current)}</div>
+          <div
+            className={styles.progressBarContainer}
+            onClick={handleProgressBarClick}
+          >
+            <div
+              className={styles.progressBar}
+              style={{ width: `${(current / duration) * 100}%` }}
+            ></div>
+          </div>
+          <div className={styles.time}>{formatTime(duration)}</div>
+        </div>
 
         <div className={styles.musicNavigation}>
           <FontAwesomeIcon icon={faBackward} />
@@ -36,8 +121,8 @@ const MusicPlayer = ({ music }) => {
               background: `linear-gradient(to right, ${music?.color?.[0]}, ${music?.color?.[1]})`,
             }}
           >
-            <div className={styles.playWrapper}>
-              <FontAwesomeIcon icon={faPlay} />
+            <div className={styles.playWrapper} onClick={togglePlayback}>
+              <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
             </div>
           </div>
           <FontAwesomeIcon icon={faForward} />
